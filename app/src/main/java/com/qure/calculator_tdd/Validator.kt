991e76b06da88f908bee9fd4b6270c004af9c28c
dltkd1395 <dltkd13956@naver.com>
2023-01-07 21:50:22 +0900

    docs(REAME.md) : 계산기 요구사항 업데이트

--- app/src/main/java/com/qure/calculator_tdd/Validator.kt
+++
@@ -1,6 +0,0 @@
-package com.qure.calculator_tdd
-
-class Validator {
-    fun isFirstOperator(value: String, regex: Regex): Boolean =
-        regex.matches(value.toList().first().toString())
-}
--- app/src/main/java/com/qure/calculator_tdd/domain/CalculatorState.kt
+++
@@ -1,6 +0,0 @@
-package com.qure.calculator_tdd.domain
-
-sealed class CalculatorState {
-
-    data class Match(val value: List<String>): CalculatorState()
-}
